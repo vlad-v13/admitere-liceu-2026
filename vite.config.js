b63b080ee249870a6c706c5635a68f773c787b86
Vlad-V13 <contact@rvv.ro>
2026-07-09 07:37:18 +0300

    Configureaza Vite pentru GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/admitere-liceu-2026/',
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 500,
+    },
+  },
+})
