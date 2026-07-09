5ec70cb39dcbd10f9ea2baa57cbbbd696cf277cb
Vlad-V13 <contact@rvv.ro>
2026-07-09 07:54:20 +0300

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   base: '/admitere-liceu-2026/',
